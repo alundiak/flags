1956a9f59b3f0faba7835532115f1a92dfd7db19
Andrii Lundiak <landike@gmail.com>
2024-10-21 23:07:06 +0200

    v1.8 (A vs B for JSON data)

--- flags.js
+++ flags.js
@@ -3,36 +3,49 @@ async function getCountryFlags() {
     const response = await fetch('https://restcountries.com/v3.1/all');
     const countries = await response.json();
 
-    const flags = {};
+    const composedFlagsData = {};
     countries.forEach(country => {
       const region = country.region;
-      flags[region] = flags[region] || {};
+      composedFlagsData[region] = composedFlagsData[region] || {};
       const code = country.cca2.toLowerCase();
-      flags[region][code] = {
-        name: country.name.common,
-        flag: country.flag
+      composedFlagsData[region][code] = {
+        // vA
+        // name: country.name.common,
+        // flag: country.flag
+
+        // vB
+        [country.flag]: country.name.common
       };
     });
 
-    return flags;
+    return composedFlagsData;
   } catch (error) {
     console.error('Error fetching country flags:', error);
     return {};
   }
 }
 
-function createFlagButtons(flags) {
+function createFlagButtons(flagsData) {
   const container = document.getElementById('flags-container');
-  for (const [region, regionObject] of Object.entries(flags)) {
+  for (const [region, regionObject] of Object.entries(flagsData)) {
     //  <p> or <span> or <tr>
     for (const [code, codeObject] of Object.entries(regionObject)) {
-      const button = document.createElement('span');
-      button.textContent = `${codeObject.flag}`;
-      button.title = `${codeObject.name}`;
-      button.onclick = () => copyToClipboard(codeObject);
-      // const span = document.createElement('span');
-      // span.textContent = `${codeObject.name}`;
-      container.appendChild(button);
+      const spanElement = document.createElement('span');
+
+      // vA
+      // spanElement.textContent = `${codeObject.flag}`;
+      // spanElement.title = `${codeObject.name}`;
+
+      // vB
+      const keys = Object.keys(codeObject);
+      const flagEmoji = keys[0];
+      const countryName = codeObject[flagEmoji];
+      spanElement.textContent = `${flagEmoji}`;
+      spanElement.title = `${countryName}`;
+
+      spanElement.onclick = () => copyToClipboard(codeObject);
+
+      container.appendChild(spanElement);
     }
   }
 }
@@ -60,6 +73,7 @@ function showToast(message) {
   }, 2000);
 }
 
-getCountryFlags().then(flags => {
-  createFlagButtons(flags);
+getCountryFlags().then(flagsFromApi => {
+  // console.log(JSON.stringify(flagsFromApi));
+  createFlagButtons(flagsFromApi);
 });
